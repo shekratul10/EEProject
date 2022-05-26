169fbf2f86975f7ef8cb945e19f7e7f6360097bc
Ratul <100375884+shekratul10@users.noreply.github.com>
2022-05-26 16:46:20 +0100

    Add files via upload

---
+++ setup.ino
@@ -0,0 +1,162 @@
+/**************************************************************************************************************************************
+  EEERover Starter Example
+  
+  Based on AdvancedWebServer.ino - Simple Arduino web server sample for SAMD21 running WiFiNINA shield
+  For any WiFi shields, such as WiFiNINA W101, W102, W13x, or custom, such as ESP8266/ESP32-AT, Ethernet, etc
+  
+  WiFiWebServer is a library for the ESP32-based WiFi shields to run WebServer
+  Forked and modified from ESP8266 https://github.com/esp8266/Arduino/releases
+  Forked and modified from Arduino WiFiNINA library https://www.arduino.cc/en/Reference/WiFiNINA
+  Built by Khoi Hoang https://github.com/khoih-prog/WiFiWebServer
+  Licensed under MIT license
+  
+  Copyright (c) 2015, Majenko Technologies
+  All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+  
+  Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+  
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+  
+  Neither the name of Majenko Technologies nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************************************************************************/
+#define USE_WIFI_NINA         false
+#define USE_WIFI101           true
+#include <WiFiWebServer.h>
+
+const char ssid[] = "EEERover";
+const char pass[] = "exhibition";
+const int groupNumber = 5; // Set your group number to make the IP address constant - only do this on the EEERover network
+
+//Webpage to return when root is requested
+const char webpage[] = R====("<html><head><style>
+.btn {background-color: inherit;padding: 14px 28px;font-size: 16px;}
+.btn:hover {background: #eee;}
+</style></head>
+<body>
+<button class="btn" onclick="ledOn()">LED On</button>
+<button class="btn" onclick="ledOff()">LED Off</button>
+<button class="btn" onclick="forward()">Forward</button>
+<br>LED STATE: <span id="state">OFF</span>
+<br>MOTION STATE: <span id="state">OFF</span>
+</body>
+<script>
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {document.getElementById("state").innerHTML = this.responseText;}};
+function ledOn() {xhttp.open("GET", "/on"); xhttp.send();}
+function ledOff() {xhttp.open("GET", "/off"); xhttp.send();}
+function forward() {xhttp.send();}
+</script></html>";)
+
+
+WiFiWebServer server(80);
+
+//Return the web page
+void handleRoot()
+{
+  server.send(200, F("text/html"), webpage);
+}
+
+//Switch LED on and acknowledge
+void ledON()
+{
+  digitalWrite(LED_BUILTIN,1);
+  server.send(200, F("text/plain"), F("ON"));
+}
+
+//Switch LED on and acknowledge
+void ledOFF()
+{
+  digitalWrite(LED_BUILTIN,0);
+  server.send(200, F("text/plain"), F("OFF"));
+}
+
+}
+//Generate a 404 response with details of the failed request
+void handleNotFound()
+{
+  String message = F("File Not Foundnn"); 
+  message += F("URI: ");
+  message += server.uri();
+  message += F("nMethod: ");
+  message += (server.method() == HTTP_GET) ? F("GET") : F("POST");
+  message += F("nArguments: ");
+  message += server.args();
+  message += F("n");
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "n";
+  }
+  server.send(404, F("text/plain"), message);
+}
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, 0);
+
+  Serial.begin(9600);
+
+  //Wait 10s for the serial connection before proceeding
+  //This ensures you can see messages from startup() on the monitor
+  //Remove this for faster startup when the USB host isn't attached
+  while (!Serial && millis() < 10000);  
+
+  Serial.println(F("nStarting Web Server"));
+
+  //Check WiFi shield is present
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
+    Serial.println(F("WiFi shield not present"));
+    while (true);
+  }
+
+  //Configure the static IP address if group number is set
+  if (groupNumber)
+    WiFi.config(IPAddress(192,168,0,groupNumber+1));
+
+  // attempt to connect to WiFi network
+  Serial.print(F("Connecting to WPA SSID: "));
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print('.');
+  }
+
+  //Register the callbacks to respond to HTTP requests
+  server.on(F("/"), handleRoot);
+  server.on(F("/on"), ledON);
+  server.on(F("/off"), ledOFF);
+
+  server.onNotFound(handleNotFound);
+  
+  server.begin();
+  
+  Serial.print(F("HTTP server started @ "));
+  Serial.println(static_cast<IPAddress>(WiFi.localIP()));
+}
+
+//Call the server polling function in the main loop
+void loop()
+{
+  server.handleClient();
+}
