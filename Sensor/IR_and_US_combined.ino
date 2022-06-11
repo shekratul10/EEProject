a3f516da8533fe77202fe9ebaba354e27a594151
an521 <106095203+an521@users.noreply.github.com>
2022-06-11 16:27:05 +0100

    Update IR_and_US_combined.ino

--- Sensor/IR_and_US_combined.ino
+++ Sensor/IR_and_US_combined.ino
@@ -25,12 +25,16 @@ void IR() {
   freq = 1000000/period;
   if(ontime != 0) {
     if(freq > 340.91 && freq < 366.78) {
-      Serial.println("IR Pulse, 353 Hz");
+      // Serial.println("IR Pulse, 353 Hz");
+      // 353 Hz IR pulse detected
     }
     else if(freq > 564.97 && freq < 585.28) {
-      Serial.println("IR Pulse, 571 Hz");
+      // Serial.println("IR Pulse, 571 Hz");
+      // 571 Hz IR pulse detected
     }
   }
+  else {
+    // No IR pulse detected
 }
 
 void US(){
@@ -39,6 +43,10 @@ void US(){
   period = offtime + ontime;
   freq = 1000000/period;
   if(freq > 37037.03 && 38461.54) {
-    Serial.println("Acoustic signal, 40 kHz");
+    // Serial.println("Acoustic signal, 40 kHz");
+    // Acoustic signal detected
+  }
+  else {
+    // no acoustic signal detected - make the equivalent for web interface
   }
 }
