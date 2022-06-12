8c2d7746d216d53324dec739efefc1a6784258de
an521 <106095203+an521@users.noreply.github.com>
2022-06-12 16:06:17 +0100

    update to sensor code

--- Sensor/IR_US_and_CRadio_combined.ino
+++ Sensor/IR_US_and_CRadio_combined.ino
@@ -3,6 +3,7 @@ int ontime, offtime;
 
 void setup() {
   // once again any digital pin can be used except for pins 5, 7 & 10 (since they are for the wifi shield functionality
+  pinMode(2, INPUT); // Radio carrier frequency
   pinMode(3, INPUT); // IR
   pinMode(4, INPUT); // US
   Serial.begin(9600);
@@ -12,6 +13,7 @@ void setup() {
 void loop() {
   IR();
   US();
+  CRadio();
 
 }
 
@@ -50,3 +52,20 @@ void US(){
     // no acoustic signal detected - make the equivalent for web interface
   }
 }
+  
+void CRadio(){
+  // this function mostly works without filters, but they may be necessary since these signals have very high frequencies. 
+  ontime = pulseIn(2, HIGH);
+  offtime = pulseIn(2, LOW);
+  period = offtime + ontime;
+  freq = 1000000/period;
+  if(ontime != 0){
+    if(freq > 55555.55 && freq < 58823.53){
+      Serial.println("Radio carrier, 61 kHz");
+    }
+    else if(freq > 83333.34 && freq < 90909.1){
+      Serial.println("Radio carrier, 89 kHz");
+    }
+  }
+}
+  
