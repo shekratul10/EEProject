65519e4f99f22a07903ea5ecbd6f346805deaa76
an521 <106095203+an521@users.noreply.github.com>
2022-06-19 12:38:44 +0100

    Update carrier.ino

--- Sensor/Radio Wave/carrier.ino
+++ Sensor/Radio Wave/carrier.ino
@@ -1,16 +1,16 @@
-int analogPin1 = A0; // potentiometer wiper (middle terminal) connected to analog pin 3
-                    // outside leads to ground and +5V
 int val1 = 0; // variable to store the value read
-int a;
 void setup() {
   Serial.begin(9600);           //  setup serial
+  pinMode(A0, INPUT);
 }
 
 void loop() {
-  val1 = analogRead(analogPin1);  // read the input pin
+  val1 = analogRead(A0);  // read the input pin
 
   float volt1 = val1 * (5000 / 1024);
-  if(volt1 > 500 ){
+  if(volt1 > 500 ){ // If the voltage input at this pin is greater than a certain threshold then an 89 kHz signal is detected
+    // the amplitudes of 61 kHz and 89 kHz radio waves are different to each other in the amplitude modulation part, allowing differentiation between the two
+    // only the 89 kHz method is shown here since this is the resonant circuit that has been implemented. 
   Serial.println("89kHz carrier frequency signal are detected");
  } 
 
